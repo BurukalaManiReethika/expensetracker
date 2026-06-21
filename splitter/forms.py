0b4e7d4c1b246ebf4ae80a2b0da3c5c94250f790
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-06-21 19:10:00 +0530

    Update forms.py

--- splitter/forms.py
+++ splitter/forms.py
@@ -2,7 +2,24 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import ExpenseGroup
 
+# Add at top
+from django.contrib.auth.forms import UserCreationForm
 
+# Add this class
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'class': 'form-control', 'placeholder': 'Email'
+    }))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Username'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirm Password'})
 class GroupCreateForm(forms.ModelForm):
     class Meta:
         model = ExpenseGroup
