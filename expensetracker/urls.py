ae2379ac61c388df1a4326c223fb4dd360b6fc32
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-06-21 19:11:47 +0530

    Update urls.py

--- expensetracker/urls.py
+++ expensetracker/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from splitter import views as splitter_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', splitter_views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='splitter/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('splitter.urls')),
